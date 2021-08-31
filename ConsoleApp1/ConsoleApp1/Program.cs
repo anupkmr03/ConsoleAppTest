c6572af32eac59519e568cc75e11c34840a57eaa
Anup Kumar <anukumar@microsoft.com>
2021-08-31 20:24:13 +0530

    Test App

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        //static void Main(string[] args)
+        //{
+        //string file = "fe4ba9ee-7061-4406-b8b9-256ccae28184/uvp/aea1c57b-aeaf-45f4-96e6-8d3558ca9ff4";
+        //     download_FromBlob(file, "uvpcontainer");
+        // }
+
+        public static void download_FromBlob(string filetoDownload, string azure_ContainerName)
+        {
+            Console.WriteLine("Inside downloadfromBlob()");
+
+            string storageAccount_connectionString = "DefaultEndpointsProtocol=https;AccountName=anupstorage;AccountKey=3sdoBXltauO0QFWrKmKpN74heus1iziHLh+mCzetjLqeUmKn9bSEuARqQuBVRGO715cm7P7lilbCV1p2kvq4RA==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount mycloudStorageAccount = CloudStorageAccount.Parse(storageAccount_connectionString);
+            CloudBlobClient blobClient = mycloudStorageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(azure_ContainerName);
+            CloudBlockBlob cloudBlockBlob = container.GetBlockBlobReference(filetoDownload);
+
+            // provide the file download location below            
+            Stream file = File.OpenWrite("C:\\Users\\anukumar\\Downloads\\test.vsix");
+
+            cloudBlockBlob.DownloadToStream(file);
+
+            Console.WriteLine("Download completed!");
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/UploadFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class UploadFile
+    {
+        private static void Main(string[] args)
+        {
+            string filename = @"D:\format-toggle-3.0.0.vsix";
+            FileStream fs = File.OpenRead(filename);
+            fs.Seek(0, SeekOrigin.Begin);
+            _ = UploadArtifactFileAsync(fs);
+            fs.Position = 0;
+            //Re-use the same stream.
+        }
+
+        public static async Task UploadArtifactFileAsync(Stream ArtifactFileStream)
+        {
+            if (ArtifactFileStream == null)
+            {
+                throw new ArgumentNullException(nameof(ArtifactFileStream));
+            }
+            try
+            {
+                // PMP config
+                const string targetHost = "pmp-dev-wcus-webapi.azurewebsites.net";
+                const string registryName = "vscode";
+                const string artifactFileTypeMoniker = "vscodevsix";
+
+                // Construct Request
+                string requestUri = $"https://{targetHost}/api/upload?" +
+                    $"RegistryName={registryName}&ArtifactFileTypeMoniker={artifactFileTypeMoniker}";
+                HttpClient httpClient = new HttpClient();
+                StreamContent streamContent = new StreamContent(ArtifactFileStream);
+                MultipartFormDataContent content = new MultipartFormDataContent
+                {
+                        { streamContent, "artifactFile", "artifactFile" }
+                };
+
+                // Perform Request
+                HttpResponseMessage response = httpClient.PostAsync(requestUri, content).Result;
+
+                // Process Response
+                Console.WriteLine(response.StatusCode);
+                Console.WriteLine(response.Content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
